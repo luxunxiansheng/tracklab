82d6ede77c854156d771f45b32bfcc0d0d435bcf
OrNot <bin.li@kickerlab.com>
2025-09-23 09:47:38 +0000

    refactor the project structure

--- tracklab/pipeline/__init__.py
+++ tracklab/pipeline/__init__.py
@@ -1,6 +1,6 @@
 from .module import Module, Pipeline, Skip
 from .detectionlevel_module import DetectionLevelModule
-from .evaluator import Evaluator
+from ..eval.evaluator import Evaluator
 from .imagelevel_module import ImageLevelModule
 from .videolevel_module import VideoLevelModule
 
