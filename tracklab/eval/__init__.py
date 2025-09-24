0f66e8e156dbd75deb88ba176c1e5a71721fa952
OrNot <bin.li@kickerlab.com>
2025-09-24 04:25:38 +0000

    add /tracklab/eval into git

---
+++ tracklab/eval/__init__.py
@@ -0,0 +1,10 @@
+from .posetrack import PoseTrack18Evaluator, PoseTrack21Evaluator
+from .trackeval_evaluator import TrackEvalEvaluator
+from .detection_only_evaluator import DetectionOnlyEvaluator
+
+__all__ = [
+    "PoseTrack18Evaluator",
+    "PoseTrack21Evaluator",
+    "TrackEvalEvaluator",
+    "DetectionOnlyEvaluator",
+]
